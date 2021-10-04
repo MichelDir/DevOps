c7d3c9fa268c9cfece6df1e541d1cc75c8a0be22
root <root@master2.us-west4-b.c.linear-equator-326806.internal>
2021-10-04 12:48:09 +0000

    DZ 8

---
+++ dz_8/git_add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+now=$(date +'%m-%d-%Y-%s')
+echo $now
+git config --global user.name "MichelDir"
+git config --global user.email "mcherepahin@gmail.com"
+cd /tmp/checkout/target
+git init
+git add *.war
+git commit -m "bild $now"
+git push --set-upstream https://micheldir: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@github.com/MichelDir/prod.git
