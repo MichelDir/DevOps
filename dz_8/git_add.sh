69177613933dc9fb386e083ad2a87136e628954a
MichelDir <91084447+MichelDir@users.noreply.github.com>
2021-10-25 12:14:06 +0300

    Update git_add.sh

--- dz_8/git_add.sh
+++ dz_8/git_add.sh
@@ -2,10 +2,10 @@
 now=$(date +'%m-%d-%Y-%s')
 echo $now
 git config --global user.name "MichelDir"
-git config --global user.email "mcherepahin@gmail.com"
+git config --global user.email "i@gmail.com"
 cd /tmp/checkout/target
 git init
 git add *.war
 git commit -m "bild $now"
-git push --set-upstream https://micheldir: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@github.com/MichelDir/prod.git
+git push --set-upstream https://micheldir@github.com/MichelDir/prod.git
 rm -rf /tmp/checkout
