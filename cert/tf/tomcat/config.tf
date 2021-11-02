fc4f4c21c2e693a1ec16e050c0f860a81e846564
root <root@cert1jenkins.us-west4-b.c.linear-equator-326806.internal>
2021-11-02 22:06:11 +0000

    cert 10

---
+++ cert/tf/tomcat/config.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+provider "google" {
+  region = "us-west4"
+  zone = "us-west4-b"
+  project = "linear-equator-326806"
+}
+
+resource "google_compute_instance" "vm_instance_tomcat" {
+  name         = "terraform-tomcat"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "831215408142-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
+
+
+resource "null_resource" "ansible_tomcat" {
+ provisioner "remote-exec" {
+    connection {
+      host = "terraform-tomcat"
+      user = "ubuntu"
+      private_key = file("./key.pvk")
+    }
+
+    inline = ["echo 'connected!'"]
+ }
+}
